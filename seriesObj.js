40cedd2215ce3dbfba17ceac65efe9eb40ff55f1
unknown <daniel.torres.84@gmail.com>
2019-05-01 12:39:41 -0700

    cloned dev branch to work computer and started working on Series Object excercise

--- seriesObj.js
+++ seriesObj.js
@@ -1,1 +1,20 @@
 const async = require('async');
+const http = require('http');
+
+const url1 = process.argv[2];
+const url2 = process.argv[3];
+
+
+
+
+
+
+
+
+
+
+
+
+/*----Doc: Async.Series(https://caolan.github.io/async/docs.html#series)---
+
+*/
